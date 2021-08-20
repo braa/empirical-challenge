65d33f060935287910a93eddb76a95290567d71d
braian <braian.dazz@gmail.com>
2021-08-20 00:01:00 -0300

    asd

---
+++ src/pages/Converter.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+import { Col, Row, Typography } from 'antd';
+
+const { Title, Text } = Typography;
+
+const Converter: FunctionComponent = () => (
+  <div>
+      <Title>Cryptocurrency Converter Calculator</Title>
+
+    Converter content
+   
+  </div>
+);
+
+export default Converter
